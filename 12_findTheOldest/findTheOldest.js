7986ceb94fceae2ca283447275c5fe1ee7358ce1
Britney Gwynne <britneygwynne@MacBook-Pro.modem>
2021-12-06 19:30:18 +1000

    finished 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const currentYear = (new Date()).getFullYear();
+    sorted = array.sort((a, b) => {
+        if(!a.yearOfDeath){
+            a.yearOfDeath = currentYear;
+        }
+        if(!b.yearOfDeath){
+            b.yearOfDeath = currentYear;
+        }
+        ageA = a.yearOfDeath - a.yearOfBirth;
+        ageB = b.yearOfDeath - b.yearOfBirth;
+        if(ageA < ageB){
+            return 1;
+        } else {
+            return -1;
+        }
+    });
+    return sorted[0]
 
 };
 
